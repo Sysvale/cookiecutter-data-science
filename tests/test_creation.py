5e0474be6718c990edf3f0dfe8e6621be9614ebf
elaynelemos <elayne.l.lemos@gmail.com>
2020-06-05 17:53:24 -0300

    Removes makefile test

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -78,11 +78,6 @@ class TestCookieSetup(object):
                 lines = list(map(lambda x: x.strip(), fin.readlines()))
             assert 'pathlib2' in lines
 
-    def test_makefile(self):
-        makefile_path = self.path / 'Makefile'
-        assert makefile_path.exists()
-        assert no_curlies(makefile_path)
-
     def test_folders(self):
         expected_dirs = [
             'data',
